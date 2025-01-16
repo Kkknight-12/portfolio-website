ae91d48c06be98253a4abce2a45c818ad8cf8a98
Kkknight-12 <mayanks365@gmail.com>
2025-01-16 20:33:49 +0530

    Refactor interactiveTerminal to update developer name to knight

--- app/(home)/_components/interactiveTerminal.tsx
+++ app/(home)/_components/interactiveTerminal.tsx
@@ -148,7 +148,7 @@ export function InteractiveTerminal() {
           <div className='w-3 h-3 rounded-full bg-green-500' />
         </div>
         <div className='flex-1 text-center text-sm text-gray-400'>
-          developer@portfolio ~
+          knight@portfolio ~
         </div>
       </div>
 
