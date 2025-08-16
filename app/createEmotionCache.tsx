19680d47df55ea9841d84a4da3e6b04327784c4a
Kkknight-12 <mayanks365@gmail.com>
2025-08-17 02:10:01 +0530

    refactor: clean up codebase and remove unused files
    - Remove all commented-out old code from experience page
    - Remove old MUI Navbar implementation
    - Delete unused MUI theme files (createEmotionCache, theme/index)
    - Delete unused ModeSwitch button component
    - Remove sample.tsx with only commented code
    - Clean up 139 lines of commented code from Navbar
    - Clean up 63 lines of commented code from experience page
    - Remove redundant CardComparison function

--- app/createEmotionCache.tsx
+++
@@ -1,9 +0,0 @@
-// "use client";
-
-// import createCache from "@emotion/cache";
-
-// export default function createEmotionCache() {
-//   return createCache({ key: "css" });
-// }
-
-// export const cache = createEmotionCache();
